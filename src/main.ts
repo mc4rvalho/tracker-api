d45672fa4941e4c70997ca0d3344de590052ca2e
mc4rvalho <mcarvalho093@gmail.com>
2026-05-04 19:23:46 -0300

    feat(tracker): add class-validator and global validation pipe for dtos

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   process.env.TZ = '-03:00';
 
-  app.useGlobalPipes();
+  app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();
 
--- src/tracker/dto/create-tracker.dto.ts
+++ src/tracker/dto/create-tracker.dto.ts
@@ -1,5 +1,15 @@
+import { IsInt, IsNotEmpty, IsString, Max, Min } from 'class-validator';
+
 export class CreateTrackerDto {
+  @IsString()
+  @IsNotEmpty()
   titulo!: string;
+
+  @IsString()
   categoria!: string;
+
+  @IsInt()
+  @Min(0)
+  @Max(10)
   nota!: number;
 }
